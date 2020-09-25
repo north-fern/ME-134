607d8f25ffe4545765549c07f3fec43b623166c8
north-fern <60278154+north-fern@users.noreply.github.com>
2020-09-25 19:17:52 -0400

    Update analogClock.py

--- analogClock.py
+++ analogClock.py
@@ -72,6 +72,7 @@ def rapid_time():
     while True:
             set_hour_servo(hour, minute)
             set_minute_servo(minute)
+            time.sleep(1)
             minute = minute + 1
             if minute == 60:
                 hour = hour + 1
