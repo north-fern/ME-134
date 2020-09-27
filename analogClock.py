2da3bcd3837d75676bd4837b74550fd6e1e2819d
north-fern <60278154+north-fern@users.noreply.github.com>
2020-09-27 15:46:15 -0400

    Update analogClock.py

--- analogClock.py
+++ analogClock.py
@@ -19,6 +19,12 @@ minuteServo = servo.Servo(pca.channels[6])
 ######################
 #function definitions#
 ######################
+
+'''
+This function takes the minutes and hours value from the date-time and sets the hour
+hand to the proper angle based on how far along the hour it is i.e. if the time is closer to
+6 than 5, aka 5:45, the hour indicator will be closer to the 6 notation that the 5 notation
+'''
 def set_hour_servo(hour, minute):
     print("Doing Something HOURS")
     if hour > 12:
@@ -50,12 +56,19 @@ def set_hour_servo(hour, minute):
         hourServo.angle = (angle)
         print(angle)
 
+'''
+This function takes the minutes value from the date-time and sets the angle of the 
+minute-controling servo to 3* the minute number
+'''
 def set_minute_servo(minute):
     print("Doing Something Else MINUTES")
     angle = minute*3
     minuteServo.angle = (angle)
     print(angle)
 
+''' 
+This function takes the time on the clock and runs it as normal
+'''
 def normal_time():
     while True:
         timeNOW = time.localtime()
@@ -64,8 +77,14 @@ def normal_time():
         print("The time is: " + str(hour) + ":" + str(minute))
         set_hour_servo(hour, minute)
         set_minute_servo(minute)
+ 
         time.sleep(10)
 
+
+'''
+This function progresses at 4 times the normal rate of a clock 
+to demonstrate the functionality of the clock
+'''
 def rapid_time():
     hour = 0
     minute = 0
@@ -79,6 +98,12 @@ def rapid_time():
                 minute = 0
             if hour == 13:
                 hour = 1
+
+
+'''
+This function should demonstrate the clock moving to a specific time, 6:30
+
+'''
 
 def preset()
     set_hour_servo(6,30)
