3220a3d649bcbc155c08f67f1ba02d2ca944cee7
north-fern <60278154+north-fern@users.noreply.github.com>
2020-09-27 17:46:48 -0400

    Create servoTest.py

---
+++ servoTest.py
@@ -0,0 +1,30 @@
+import time
+from adafruit_motor import servo
+from board import SCL, SDA
+import busio
+from adafruit_pca9685 import PCA9685
+
+i2c_bus  = busio.I2C(SCL, SDA)
+
+pca = PCA9685(i2c_bus)
+
+pca.frequency = 50
+
+servo0 = servo.Servo(pca.channels[3])
+servo1 = servo.Servo(pca.channels[6])
+print("STARTING TEST")
+
+for i in range(180):
+        servo0.angle = i
+        servo1.angle = i
+        time.sleep(.1)
+
+print("ENDING TEST")
+
+for i in range(180):
+        servo0.angle = 180-i
+        servo1.angle = 180 - i
+        time.sleep(.1)
+
+print("TEST COMPLETE")
+pca.deinit()
