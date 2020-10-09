60e504c5a428cc3ebcf9f6f07613bfa5ad8dba74
north-fern <60278154+north-fern@users.noreply.github.com>
2020-10-09 12:53:47 -0400

    Create letters.py

---
+++ letters.py
@@ -0,0 +1,85 @@
+letters
+
+
+## MOTION FUNCTIONS ##
+def np = up_one(x, y, WRITE_BOOL):
+
+def np = down_one(x, y, WRITE_BOOL):
+
+def np = left_one(x, y, WRITE_BOOL):
+
+def np = right one(x, y, WRITE_BOOL):
+
+def np = down_diag_l2r(x, y, WRITE_BOOL):
+
+def np = down_diag_r2l(x, y, WRITE_BOOL):
+
+def np = up_diag_l2r(x, y, WRITE_BOOL):
+
+def np = up_diag_r2l(x, y, WRITE_BOOL):
+
+
+## LETTER DEFINITONS ##
+def A(x, y):
+
+def B(x, y):
+    
+def C(x, y):
+
+def E(x, y):
+
+def F(x, y):
+
+def G(x, y):
+
+def H(x, y):
+
+def I(x, y):
+
+def J(x, y):
+
+def K(x, y):
+
+def L(x, y):
+
+def M(x, y):
+
+
+def N(x, y):
+
+
+def O(x, y):
+
+
+def P(x, y):
+
+
+def Q(x, y):
+
+
+def R(x, y):
+
+
+def S(x, y):
+
+
+def T(x, y):
+
+
+def U(x, y):
+
+
+def V(x, y):
+
+
+def W(x, y):
+
+
+def X(x, y):
+
+
+def Y(x, y):
+
+
+def Z(x, y):
+
