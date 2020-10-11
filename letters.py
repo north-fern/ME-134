70bfee6b768e9a6fc2b7f58caecc0cfb4f1a7f3d
north-fern <60278154+north-fern@users.noreply.github.com>
2020-10-11 18:15:43 -0400

    Update letters.py

--- letters.py
+++ letters.py
@@ -651,19 +651,19 @@ time.sleep(.5)
 elbow.angle = t2
 time.sleep(.5)
 
-t1,t2 = getAngles(5,0)
+t1,t2 = getAngles(15,0)
 shoulder.angle = t1
 time.sleep(.5)
 elbow.angle = t2
 time.sleep(.5)
 
-t1,t2 = getAngles(5,5)
+t1,t2 = getAngles(15,15)
 shoulder.angle = t1
 time.sleep(.5)
 elbow.angle = t2
 time.sleep(.5)
 
-t1,t2 = getAngles(0,5)
+t1,t2 = getAngles(0,15)
 shoulder.angle = t1
 time.sleep(.5)
 elbow.angle = t2
