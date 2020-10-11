b0c2f5a8d9fb8d973bca43307225cb6461ca99c5
north-fern <60278154+north-fern@users.noreply.github.com>
2020-10-11 15:06:51 -0400

    Update servoTest.py

--- servoTest.py
+++ servoTest.py
@@ -11,12 +11,12 @@ pca = PCA9685(i2c_bus)
 pca.frequency = 50
 
 servo0 = servo.Servo(pca.channels[0])
-#servo1 = servo.Servo(pca.channels[6])
+servo1 = servo.Servo(pca.channels[3])
 print("STARTING TEST")
 
 for i in range(180):
         servo0.angle = i
-       # servo1.angle = i
+        servo1.angle = i
         time.sleep(.1)
 stop = input("CONTINUE TEST? ")
 
@@ -24,7 +24,7 @@ print("ENDING TEST")
 
 for i in range(180):
         servo0.angle = 180-i
-        #servo1.angle = 180 - i
+        servo1.angle = 180 - i
         time.sleep(.1)
 
 print("TEST COMPLETE")
