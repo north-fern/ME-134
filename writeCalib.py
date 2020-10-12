201a52376d0d40aaca9f094ab785817a50b22e57
north-fern <60278154+north-fern@users.noreply.github.com>
2020-10-11 23:35:31 -0400

    Update writeCalib.py

--- writeCalib.py
+++ writeCalib.py
@@ -187,9 +187,9 @@ elbow.angle = (18/13)*t2
 
 
 while True:
-   x = float(input("X Coord? "))
-   y = float((input("Y Coord? "))
-   theta1, theta2 = getAngles(x,y)
+   x = float((input("X Coord? ")))
+   y = float((input("Y Coord? ")))
+   theta1 , theta2 = getAngles(x,y)
    shoulder.angle = theta1
    elbow.angle = theta2
 
