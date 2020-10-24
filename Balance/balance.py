da19a143ab294b6e8e2e98b150ba90418f27b510
north-fern <60278154+north-fern@users.noreply.github.com>
2020-10-24 14:05:50 -0400

    added comments

--- Balance/balance.py
+++ Balance/balance.py
@@ -1,3 +1,16 @@
+'''
+This code is designe for a balancing robot. The three servos are placed in 120* separations from each other
+around the edge of a styrofoam ball. They move randomly and check to see if they are closer to being balanced. 
+The IMU is placed in the center of the ball and the program (imusensor library) calculates a roll, pitch and yaw.
+If the pi senses that the difference between the goal points and new points is better, it keeps the new motor angle. 
+if it isn't it goes back to the previous angle. The angle additon is randomly generated. 
+
+'''
+
+
+'''
+Imports
+'''
 import FaBo9Axis_MPU9250 
 import time
 import sys
@@ -8,9 +21,13 @@ import random2 as random
 from adafruit_motor  import servo
 import os
 import smbus
-
 from imusensor.MPU9250 import MPU9250
 
+
+'''
+setup of IMU and begining calculations
+
+'''
 address = 0x68
 bus = smbus.SMBus(1)
 imu = MPU9250.MPU9250(bus, address)
@@ -20,38 +37,52 @@ imu.computeOrientation()
 goal_x = imu.roll
 goal_y = imu.pitch
 
+'''
+setting up motor driver
+'''
 i2c = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
 
-
-## servo setup
-
+'''
+servo setup
+'''
 s1 = servo.Servo(pca.channels[0])
 s2 = servo.Servo(pca.channels[3])
 s3 = servo.Servo(pca.channels[6])
 
+'''
+initialize current angles of servo
+'''
 s1_curt_ang = 0
 s2_curt_ang = 0
 s3_curt_ang = 0
 
 
 while True:
+
+    ##randomly choose motor
     motor = random.randint(0,2)
     print("Random motor choice is :" + str(motor))
+
+    ## randomly choose angle to change by
     angleDelta = random.randint(-15, 15)
     imu.readSensor()
     imu.computeOrientation()
-
     print("M1: " + str(s1_curt_ang) + " M2: " + str(s2_curt_ang) + " M3: " + str(s3_curt_ang))
 
-    pre_diff_pitch = 100
-    pre_diff_roll = 100
+    ##set initial differences, make them large
+    pre_diff_pitch = 1000
+    pre_diff_roll = 1000
+
+    ## setup of motor selection
     if motor == 0:
         if (s1_curt_ang + angleDelta) >= 0 and (s1_curt_ang + angleDelta)  <= 180:
+            # check to see if angle is out of range
             s1.angle = s1_curt_ang + angleDelta
             time.sleep(.1)
             if abs(imu.roll - goal_x) < pre_diff_pitch or abs(imu.roll - goal_y) < pre_diff_roll:
+                #check to see if new value is better; if it is, change baselines
                 pre_diff_pitch = abs(imu.pitch - goal_x)
                 pre_diff_roll = abs(imu.roll - goal_y)
                 s1_curt_ang = s1_curt_ang + angleDelta
@@ -61,11 +92,14 @@ while True:
         else:
             s1.angle = s1_curt_ang
             time.sleep(.1)
+    
     if motor == 1:
         if (s2_curt_ang + angleDelta) >= 0 and (s2_curt_ang + angleDelta)  <= 180:
+            # check to see if angle is out of range
             s2.angle = s2_curt_ang + angleDelta
             time.sleep(.1)
             if abs(imu.pitch - goal_x) < pre_diff_pitch or abs(imu.roll - goal_y) < pre_diff_roll:
+                #check to see if new value is better; if it is, change baselines
                 pre_diff_pitch = abs(imu.pitch - goal_x)
                 pre_diff_roll = abs(imu.roll - goal_y)
                 s1_curt_ang = s2_curt_ang + angleDelta
@@ -75,11 +109,14 @@ while True:
         else:
             s2.angle = s2_curt_ang
             time.sleep(.1)
+    
     if motor == 2:
         if (s3_curt_ang + angleDelta) >= 0 and (s3_curt_ang + angleDelta)  <= 180:
+            # check to see if angle is out of range
             s3.angle = s3_curt_ang + angleDelta
             time.sleep(.1)
             if abs(imu.pitch - goal_x) < pre_diff_pitch or abs(imu.roll - goal_y) < pre_diff_roll:
+                #check to see if new value is better; if it is, change baselines
                 pre_diff_pitch = abs(imu.pitch - goal_x)
                 pre_diff_roll = abs(imu.roll - goal_y)
                 s1_curt_ang = s3_curt_ang + angleDelta
