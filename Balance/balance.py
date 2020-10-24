970abf280fe96a8241a6b5b7193637606e8fdd2e
north-fern <60278154+north-fern@users.noreply.github.com>
2020-10-24 13:33:21 -0400

    Update balance.py

--- Balance/balance.py
+++ Balance/balance.py
@@ -1,4 +1,3 @@
-from fusion import Fusion
 import FaBo9Axis_MPU9250 
 import time
 import sys
@@ -7,13 +6,21 @@ import busio
 from adafruit_pca9685 import PCA9685
 import random2
 from adafruit_motor  import servo
+import os
+import smbus
+
+from imusensor.MPU9250 import MPU9250
+
+address = 0x68
+bus = smbus.SMBus(1)
+imu = MPU9250.MPU9250(bus, address)
+imu.begin()
+
 
 i2c = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
 
-ImU = FaBo9Axis_MPU9250.MPU9250()
-fuse = Fusion()
 
 ## servo setup
 
@@ -25,49 +32,35 @@ s1_curt_ang = 0
 s2_curt_ang = 0
 s3_curt_ang = 0
 
-## setup fuse 
-acel = ImU.readAccel()
-gyr = ImU.readGyro()
-mag = IMU.readMagnet()
-fuse.update(accel, gyro, mag)
-goal_x = fuse.pitch
-goal_y = fuse.roll
-
-
-def update_vals():
-    acel = ImU.readAccel()
-    gyr = ImU.readGyro()
-    mag = IMU.readMagnet()
-    fuse.update(accel, gyro, mag)
-
-
 
 while True:
     motor = random.randrange(0,2,1)
     angleDelta = random.randrange(-5, 5, 1)
+    imu.readSensor()
+    imu.computeOrientation()
     pre_diff_pitch = 100
     pre_diff_roll = 100
     if motor == 0:
         s1.angle = s1_curt_ang + angleDelta
-        if abs(fuse.pitch - goal_x) < pre_diff_pitch or abs(fuse.roll - goal_y) < pre_diff_roll:
-            pre_diff_pitch = abs(fuse.pitch - goal_x)
-            pre_diff_roll = abs(fuse.roll - goal_y)
+        if abs(imu.roll - goal_x) < pre_diff_pitch or abs(imu.roll - goal_y) < pre_diff_roll:
+            pre_diff_pitch = abs(imu.pitch - goal_x)
+            pre_diff_roll = abs(imu.roll - goal_y)
             s1_curt_ang = s1_curt_ang + angleDelta
         else:
             s1.angle = s1_curt_ang
     if motor == 1:
         s2.angle = s2_curt_ang + angleDelta
-        if abs(fuse.pitch - goal_x) < pre_diff_pitch or abs(fuse.roll - goal_y) < pre_diff_roll:
-            pre_diff_pitch = abs(fuse.pitch - goal_x)
-            pre_diff_roll = abs(fuse.roll - goal_y)
+        if abs(imu.pitch - goal_x) < pre_diff_pitch or abs(imu.roll - goal_y) < pre_diff_roll:
+            pre_diff_pitch = abs(imu.pitch - goal_x)
+            pre_diff_roll = abs(imu.roll - goal_y)
             s1_curt_ang = s2_curt_ang + angleDelta
         else:
             s2.angle = s2_curt_ang
     if motor == 2:
         s3.angle = s3_curt_ang + angleDelta
-        if abs(fuse.pitch - goal_x) < pre_diff_pitch or abs(fuse.roll - goal_y) < pre_diff_roll:
-            pre_diff_pitch = abs(fuse.pitch - goal_x)
-            pre_diff_roll = abs(fuse.roll - goal_y)
+        if abs(imu.pitch - goal_x) < pre_diff_pitch or abs(imu.roll - goal_y) < pre_diff_roll:
+            pre_diff_pitch = abs(imu.pitch - goal_x)
+            pre_diff_roll = abs(imu.roll - goal_y)
             s1_curt_ang = s3_curt_ang + angleDelta
         else:
             s3.angle = s3_curt_ang
