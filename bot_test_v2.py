ef1134ba97a756ec8440885dabf4b39ab3586aad
north-fern <60278154+north-fern@users.noreply.github.com>
2020-11-10 11:32:09 -0500

    Create bot_test_v2.py

---
+++ bot_test_v2.py
@@ -0,0 +1,4 @@
+Python 3.7.6 (v3.7.6:43364a7ae0, Dec 18 2019, 14:12:53) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
