b00503b7d319b7627ee749dcdf37bd50b631ca5c
north-fern <60278154+north-fern@users.noreply.github.com>
2020-11-09 22:47:47 -0500

    Create crawler.py

---
+++ crawler.py
@@ -0,0 +1,68 @@
+import time
+from adafruit_motor import servo
+from board import SCL, SDA
+import busio
+from adafruit_pca9685 import PCA9685
+
+i2c_bus  = busio.I2C(SCL, SDA)
+
+pca = PCA9685(i2c_bus)
+
+pca.frequency = 50
+side = servo.Servo(pca.channels[0])
+updown = servo.Servo(pca.channels[3])
+
+side.angle = 180
+updown.angle = 30
+
+input()
+
+side.angle = 0
+
+input()
+side.angle = 90
+input()
+
+
+def left():
+    for i in range(90):
+        side.angle = 180-i
+        updown.angle = 0
+    updown.angle = 30
+
+def right():
+    for j in range(90):
+        side.angle = j
+        updown.angle = 0
+    updown.angle = 30
+
+
+while True:
+    input()
+    left()
+    side.angle = 75
+    time.sleep(.1)
+    side.angle = 55
+    time.sleep(.1)
+    side.angle = 35
+    time.sleep(.1)
+    side.angle = 0
+    time.sleep(.1)
+    side.angle = 0
+    time.sleep(.1)
+    right()
+    '''
+    side.angle = 0
+    time.sleep(.1)
+    side.angle = 0
+    time.sleep(.1)
+    side.angle = 35
+    time.sleep(.1)
+    side.angle = 55
+    time.sleep(.1)
+    side.angle = 75
+    time.sleep(.1)
+    '''
+
+pca.deinit()
+
